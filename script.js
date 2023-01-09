1acf0e24913affde5dea41b39af09de2c3704287
matheusbruno23 <matheus.bruno.1805@gmail.com>
2023-01-09 17:21:44 -0300

    Projeto com JS e WPP

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
 /*globais*/
 let finalPrice;
-
-let plateTxt;
-let drinkTxt;
-let dessertTxt;
-
+let plateTxt , drinkTxt , dessertTxt;
 let wpp;
-
-let platePrice;
-let drinkPrice;
-let dessertPrice;
+let platePrice , drinkPrice, dessertPrice;
 
 //selecionar prato//
-function selectPlate(plateSelect, nameSelector){
+function selectPlate(plateSelect, selectTxt){
     const plateAlreadySelected = document.querySelector('.plates .green');
 
     if ( plateAlreadySelected !== null){
         plateAlreadySelected.classList.remove('green');
     }
-    const selectedPlate = document.querySelector(plateSelect);
-    selectedPlate.classList.add('green');
+    plateSelect.classList.add('green');
 
-    const itemName = document.querySelector(nameSelector+ '.item-name');
-    const itemName = document.querySelector(nameSelector+ '.item-name');
+    const plateName = document.querySelector('.itemname').innerHTML;
+    plateTxt = plateName;
+    const pPrice = document.querySelector('.price').innerHTML;
+    platePrice = pPrice;
 
-    closeOrder()
+
+    openCloseOrder()
 }
 //selecionar bebida//
 function selectDrink(drinkSelect){
@@ -32,10 +27,14 @@ function selectDrink(drinkSelect){
     if ( drinkAlreadySelected !== null){
         drinkAlreadySelected.classList.remove('green');
     }
+    drinkSelect.classList.add('green');
 
-    const selectedDrink = document.querySelector(drinkSelect);
-    selectedDrink.classList.add('green');
-    closeOrder()
+    const drinkName = document.querySelector('.itemname').innerHTML;
+    drinkTxt = drinkName
+    const dPrice = document.querySelector('.price').innerHTML;
+    drinkPrice = dPrice;
+
+    openCloseOrder()
 }
 
 //selecionar sobremesa//
@@ -44,19 +43,82 @@ function selectDessert(dessertSelect){
     if(dessertAlreadySelected !== null){
         dessertAlreadySelected.classList.remove('green');
     }
-    const selectedDessert = document.querySelector(dessertSelect);
-    selectedDessert.classList.add('green');
-    closeOrder()
+    dessertSelect.classList.add('green');
+
+    const dessertName = document.querySelector('.itemname').innerHTML;
+    dessertTxt = dessertName;
+    const dsPrice = document.querySelector('.price').innerHTML;
+    dessertPrice = dsPrice;
+
+    openCloseOrder()
 }
 
-function closeOrder(){
-    plateSelected.classList.contains('green');
-    drinkSelected.classList.contains('green');
-    dessertSelected.classList.contains('green');
-
-    if(dessertSelected & drinkSelected & plateSelected === true){
-            const closeButton = document.querySelector('.closeButton');
-            closeButton.innerHTML = 'Fechar Pedido';
- 
+function openCloseOrder(){
+    if(plateTxt !== undefined){
+        if(drinkTxt !== undefined){
+            if(dessertTxt !== undefined){
+                const button = document.querySelector('.closeButton');
+                button.classList.add('close-green');
+                button.innerHTML = 'Fechar Pedido';
+                
+            }
+        }
     }
+}
+function closeOrder(){
+    if(plateTxt !== undefined){
+        if(drinkTxt !== undefined){
+            if(dessertTxt !== undefined){
+
+                platePrice = platePrice.replace('R$ ','');
+                drinkPrice = drinkPrice.replace('R$ ','');
+                dessertPrice = dessertPrice.replace('R$ ','');
+
+                platePrice = platePrice.replace(',','.');
+                platePrice = platePrice.replace(',','.');
+                platePrice = platePrice.replace(',','.');
+
+                finalPrice = Number(platePrice) + Number(drinkPrice) + Number(dessertPrice);
+
+                
+                const greyscreen = document.querySelector('.grey-screen');
+                greyscreen.classList.remove('hidden')
+
+                wpp = `Olá, gostaria de fazer o pedido:
+                Prato:${plateTxt}
+                Bebida:${drinkTxt}
+                Sobremesa:${dessertTxt}
+                Total: R$${finalPrice.toFixed(2)}`;
+
+                const elementPlate = document.querySelector('.plate .name');
+                elementPlate.innerHTML = plateTxt;
+
+                const elementPPrice = document.querySelector('.plate .price');
+                elementPPrice.innerHTML = platePrice;
+
+                const elementDrink = document.querySelector('.drink .name');
+                elementDrink .innerHTML = drinkTxt;
+                const elementPDrink = document.querySelector('.drink .price');
+                elementPDrink.innerHTML = drinkPrice;
+
+                const elementDessert = document.querySelector('.dessert .name');
+                elementDessert.innerHTML = dessertTxt;
+                const elementPDessert = document.querySelector('.dessert .price');
+                elementPDessert.innerHTML = dessertPrice;
+
+                const elementTotal = document.querySelector('.total .amount');
+                elementTotal.innerHTML = `R$ ${finalPrice.toFixed(2)}`;
+
+            }
+        }
+    }
+}
+
+function sendWpp(){
+    
+    const name = prompt('Seu nome');
+    const adress = prompt('Seu endereco');
+    wpp = wpp + `Nome: ${name} Endereco: ${adress}`;
+    const msgWP = encodeURIComponent(wpp);
+    window.open(`http://wa.me/99999999999?text=${msgWP}`);
 }
